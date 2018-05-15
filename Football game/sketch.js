39834c10406c67e95372e1cfe9500207e02eabaf
diegolbe <36966474+diegolbe@users.noreply.github.com>
2018-05-15 08:49:20 -0700

    Update sketch.js

--- Football game/sketch.js
+++ Football game/sketch.js
@@ -1,18 +1,17 @@
-//create an empty array called balls
-let points=0;
+let points= 0;
 let balls = [];
+
 function setup() {
-  createCanvas(1200, 650);
-
+   createCanvas(1200, 650);
+   angleMode(RADIANS);
 }
+
 function draw(){
-
-
-
-
-  background(38, 153, 45);
-  stroke(252, 239, 60);
-  strokeWeight(5);
+  console.log(points);
+  //make the field
+    background(38, 153, 45);
+    stroke(252, 239, 60);
+    strokeWeight(5);
     line(553.75,150,646.25,150);
     line(600,150,600,200);
     line(553.75,150,553.75,50);
@@ -28,72 +27,123 @@ function draw(){
     line(116,520.6,1084,520.6);
     line(94,600,1106,600);
 
-
-  scoreboard();
-
-
-
-
-//	draw all the balls in that array
+scoreboard();
+//	draw all the balls
 	for (let i = 0; i < balls.length; i++) {
 	    balls[i].drawBall();
-        balls[i].moveBall();
-        balls[i].score();
+      balls[i].score();
+
+
+
+
+
+
+
+// if you push enter, kick the ball
+      if(keyCode===13){
+        balls[i].kickBall();
+
+
+      }
+
 	  }
 }
 
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-    let  b = new Ball(600, 500);
-    balls.push(b);
-    console.log(balls);
+function keyPressed(){
+  //every time you push spacebar, make a new ball from the ball class and add it to the balls array
+  if (keyCode === 32){
+     let  b = new Ball(random(400,800),random(400,600),0);
+     balls.push(b);
+
+   }
+   //when you push the left arrow, aim left
+  if (keyCode === 37){
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].aimLeft();
+      }
+  }
+   //when you push the right arrow, aim right
+  if (keyCode === 39){
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].aimRight();
+      }
+
+  }
+
 }
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y, angle){ //every ball needs an x value, a y value, and an angle
 		    this.x = x;
     		this.y = y;
+        this.angle = angle;
 	}
+  drawTriangle(){  // draw a triangle on the screen at x,y
+     stroke(0);
+     fill("red");
+     push();//save canvas state
+     translate(this.x,this.y); //translate to this.x, this.y
+     rotate(this.angle/PI); // rotate
+     triangle(0, 0, 10, -20, 20, 0); // draw the triangle at 0,0- because you've moved the center
+     pop(); // pop back to normal
 
-	drawBall(){
-// draw a ball on the screen at x,y
+}
+
+  score(){
+       if (this.x >= 555 && this.x <= 645 && this.y <= 150 && this.y>= 145){
+           points = points + 3;
+       }
+     }
+
+  drawBall(){// draw a football on the screen at x,y
     		stroke(0);
         strokeWeight(1);
     		fill(135, 67, 8);
+        push();
 		    ellipse(this.x,this.y,21,33);
-         stroke(0);
-            strokeWeight(0.5);
-            line(this.x,this.y+16.5,this.x,this.y-16.5);
+        stroke(0);
+        strokeWeight(0.5);
+        line(this.x,this.y+16.5,this.x,this.y-16.5);
         stroke(255);
         strokeWeight(1.5);
         line(this.x,this.y+6,this.x,this.y-6);
         strokeWeight(1);
-              line(this.x+2,this.y+6,this.x-2,this.y+6);
+        line(this.x+2,this.y+6,this.x-2,this.y+6);
         line(this.x+2,this.y+4,this.x-2,this.y+4);
         line(this.x+2,this.y+2,this.x-2,this.y+2);
         line(this.x+2,this.y,this.x-2,this.y);
-          line(this.x+2,this.y-2,this.x-2,this.y-2);
-          line(this.x+2,this.y-4,this.x-2,this.y-4);
-            line(this.x+2,this.y-6,this.x-2,this.y-6);
-
-
-
-
+        line(this.x+2,this.y-2,this.x-2,this.y-2);
+        line(this.x+2,this.y-4,this.x-2,this.y-4);
+        line(this.x+2,this.y-6,this.x-2,this.y-6);
+        rotate(this.angle/PI);
+          translate(this.x,this.y);
+        pop();
+        fill(0);
 	}
 
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x;
+  aimLeft(){ //change the trajectory angle
+    this.angle = this.angle -.5;
+    print(this.angle);
+  }
+
+  aimRight(){ //change the trajectory angle
+    this.angle = this.angle+.5;
+    print(this.angle);
+  }
+
+	kickBall(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+this.angle;
 		this.y = this.y-5;
 	}
 
-  score(){
-     if (this.x >= 553.75 && this.x <= 646.25 && this.y >= 149 && this.y <= 151){
-         points = points + 3;
-     }
-   }
 
- }
+}
+
+
+
+
  function scoreboard(){
    stroke(0);
    strokeWeight(2);
